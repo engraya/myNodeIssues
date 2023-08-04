d80e65290cb588441a3ac8607eca8bb1af1eddab
Engraya <ahmadyakubuaya@gmail.com>
2023-08-04 20:26:04 +0100

    Templates Installation Commit

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = new express();
 
 const path = require('path');
 
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
 
 
 app.use(express.static('public'));
@@ -13,22 +17,46 @@ app.listen(5000, ()=> {
 });
 
 
+
+// Normal Render with Routes
+// app.get('/', (request, response)=> {
+//     response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+// })
+
+// app.get('/about', (request, response)=> {
+//     response.sendFile(path.resolve(__dirname, 'pages/about.html'))
+// })
+
+// app.get('/contact', (request, response)=> {
+//     response.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+// })
+
+// app.get('/portfolio', (request, response)=> {
+//     response.sendFile(path.resolve(__dirname, 'pages/portfolio.html'))
+// })
+
+// app.get('/services', (request, response)=> {
+//     response.sendFile(path.resolve(__dirname, 'pages/services.html'))
+// })
+
+
+// Rendering with ejs Templating Engine
 app.get('/', (request, response)=> {
-    response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    response.render('index')
 })
 
 app.get('/about', (request, response)=> {
-    response.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    response.render('about')
 })
 
 app.get('/contact', (request, response)=> {
-    response.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    response.render('contact')
 })
 
 app.get('/portfolio', (request, response)=> {
-    response.sendFile(path.resolve(__dirname, 'pages/portfolio.html'))
+    response.render('portfolio')
 })
 
 app.get('/services', (request, response)=> {
-    response.sendFile(path.resolve(__dirname, 'pages/services.html'))
+  response.render('services')
 })
